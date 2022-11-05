c99f0f9a6cf9c8204395c99bdae3a555fee13976
gd017897 <jessica.yu@rwth-aachen.de>
2022-11-05 01:26:39 +0100

    Added graphlet kernel and collection of graphlets with 5 nodes

---
+++ Task1/Kernels/graphlet_kernel.py
@@ -0,0 +1,27 @@
+import random
+import networkx as nx
+import numpy as np
+import graphlets
+
+"""
+G: networkx graph
+seed: seed for random sampling
+return: feature vector with shape (1, 34) as numpy array
+"""
+def graphlet_kernel(G, seed=12345):
+    random.seed(seed)
+    graphlets_list = graphlets.get_all_graphlets()
+
+    # compute histogram that holds how often graphlet occurs (repeat 1000 times)
+    feature_vector = np.zeros((1, 34))
+    for i in range(1000):
+        # sample 5 nodes to obtain sample graph
+        node_sample = random.sample(G.nodes, 5)
+        graph_sample = G.subgraph(node_sample)
+        # check which graphlet
+        for j in range(34):
+            if nx.is_isomorphic(graph_sample, graphlets_list[j]):
+                feature_vector[0, j] += 1
+                break
+
+    return feature_vector
---
+++ Task1/Kernels/graphlets.py
@@ -0,0 +1,190 @@
+import networkx as nx
+
+"""
+note that in this case we only consider graphs with 5 nodes (34 graphlets)
+return: list with 34 distinct graphlets
+"""
+def get_all_graphlets():
+    graphlets = []
+
+    # 0 edges
+    G = nx.Graph()
+    G.add_nodes_from([1, 2, 3, 4, 5])
+    graphlets.append(G)
+
+    # 1 edge
+    G = nx.Graph()
+    G.add_nodes_from([1, 2, 3, 4, 5])
+    G.add_edges_from([(1, 2)])
+    graphlets.append(G)
+
+    # 2 edges
+    G = nx.Graph()
+    G.add_nodes_from([1, 2, 3, 4, 5])
+    G.add_edges_from([(1, 2), (1, 4)])
+    graphlets.append(G)
+
+    G = nx.Graph()
+    G.add_nodes_from([1, 2, 3, 4, 5])
+    G.add_edges_from([(1, 2), (3, 4)])
+    graphlets.append(G)
+
+    # 3 edges
+    G = nx.Graph()
+    G.add_nodes_from([1, 2, 3, 4, 5])
+    G.add_edges_from([(1, 2), (1, 4), (1, 5)])
+    graphlets.append(G)
+
+    G = nx.Graph()
+    G.add_nodes_from([1, 2, 3, 4, 5])
+    G.add_edges_from([(1, 2), (1, 4), (3, 4)])
+    graphlets.append(G)
+
+    G = nx.Graph()
+    G.add_nodes_from([1, 2, 3, 4, 5])
+    G.add_edges_from([(1, 2), (3, 4), (1, 5)])
+    graphlets.append(G)
+
+    G = nx.Graph()
+    G.add_nodes_from([1, 2, 3, 4, 5])
+    G.add_edges_from([(1, 3), (1, 4), (3, 4)])
+    graphlets.append(G)
+
+    # 4 edges
+    G = nx.Graph()
+    G.add_nodes_from([1, 2, 3, 4, 5])
+    G.add_edges_from([(1, 2), (1, 3), (1, 4), (1, 5)])
+    graphlets.append(G)
+
+    G = nx.Graph()
+    G.add_nodes_from([1, 2, 3, 4, 5])
+    G.add_edges_from([(1, 2), (3, 4), (1, 4), (1, 5)])
+    graphlets.append(G)
+
+    G = nx.Graph()
+    G.add_nodes_from([1, 2, 3, 4, 5])
+    G.add_edges_from([(1, 2), (1, 3), (1, 4), (3, 4)])
+    graphlets.append(G)
+
+    G = nx.Graph()
+    G.add_nodes_from([1, 2, 3, 4, 5])
+    G.add_edges_from([(1, 2), (2, 3), (3, 4), (1, 4)])
+    graphlets.append(G)
+
+    G = nx.Graph()
+    G.add_nodes_from([1, 2, 3, 4, 5])
+    G.add_edges_from([(1, 2), (2, 3), (4, 5), (1, 4)])
+    graphlets.append(G)
+
+    G = nx.Graph()
+    G.add_nodes_from([1, 2, 3, 4, 5])
+    G.add_edges_from([(1, 5), (2, 3), (4, 5), (1, 4)])
+    graphlets.append(G)
+
+    # 5 edges
+    G = nx.Graph()
+    G.add_nodes_from([1, 2, 3, 4, 5])
+    G.add_edges_from([(1, 2), (1, 3), (1, 4), (1, 5), (3, 4)])
+    graphlets.append(G)
+
+    G = nx.Graph()
+    G.add_nodes_from([1, 2, 3, 4, 5])
+    G.add_edges_from([(1, 2), (2, 3), (1, 4), (1, 5), (3, 4)])
+    graphlets.append(G)
+
+    G = nx.Graph()
+    G.add_nodes_from([1, 2, 3, 4, 5])
+    G.add_edges_from([(1, 2), (1, 3), (1, 4), (2, 3), (3, 4)])
+    graphlets.append(G)
+
+    G = nx.Graph()
+    G.add_nodes_from([1, 2, 3, 4, 5])
+    G.add_edges_from([(1, 5), (1, 3), (1, 4), (2, 3), (3, 4)])
+    graphlets.append(G)
+
+    G = nx.Graph()
+    G.add_nodes_from([1, 2, 3, 4, 5])
+    G.add_edges_from([(1, 5), (1, 2), (1, 4), (2, 3), (4, 5)])
+    graphlets.append(G)
+
+    G = nx.Graph()
+    G.add_nodes_from([1, 2, 3, 4, 5])
+    G.add_edges_from([(1, 2), (2, 3), (3, 4), (4, 5), (1, 5)])
+    graphlets.append(G)
+
+    # 6 edges
+    G = nx.Graph()
+    G.add_nodes_from([1, 2, 3, 4, 5])
+    G.add_edges_from([(1, 2), (1, 3), (1, 4), (1, 5), (2, 3), (3, 4)])
+    graphlets.append(G)
+
+    G = nx.Graph()
+    G.add_nodes_from([1, 2, 3, 4, 5])
+    G.add_edges_from([(1, 2), (3, 5), (1, 4), (1, 5), (2, 3), (3, 4)])
+    graphlets.append(G)
+
+    G = nx.Graph()
+    G.add_nodes_from([1, 2, 3, 4, 5])
+    G.add_edges_from([(1, 2), (1, 3), (1, 4), (1, 5), (2, 3), (4, 5)])
+    graphlets.append(G)
+
+    G = nx.Graph()
+    G.add_nodes_from([1, 2, 3, 4, 5])
+    G.add_edges_from([(1, 2), (2, 3), (3, 4), (4, 5), (1, 5), (1, 4)])
+    graphlets.append(G)
+
+    G = nx.Graph()
+    G.add_nodes_from([1, 2, 3, 4, 5])
+    G.add_edges_from([(1, 2), (1, 4), (1, 5), (3, 5), (3, 4), (4, 5)])
+    graphlets.append(G)
+
+    G = nx.Graph()
+    G.add_nodes_from([1, 2, 3, 4, 5])
+    G.add_edges_from([(1, 3), (1, 4), (1, 5), (3, 5), (3, 4), (4, 5)])
+    graphlets.append(G)
+
+    # 7 edges
+    G = nx.Graph()
+    G.add_nodes_from([1, 2, 3, 4, 5])
+    G.add_edges_from([(1, 2), (1, 3), (1, 4), (1, 5), (2, 3), (3, 4), (4, 5)])
+    graphlets.append(G)
+
+    G = nx.Graph()
+    G.add_nodes_from([1, 2, 3, 4, 5])
+    G.add_edges_from([(1, 2), (1, 3), (1, 4), (1, 5), (2, 3), (3, 4), (3, 5)])
+    graphlets.append(G)
+
+    G = nx.Graph()
+    G.add_nodes_from([1, 2, 3, 4, 5])
+    G.add_edges_from([(1, 2), (1, 3), (1, 4), (1, 5), (4, 5), (3, 4), (3, 5)])
+    graphlets.append(G)
+
+    G = nx.Graph()
+    G.add_nodes_from([1, 2, 3, 4, 5])
+    G.add_edges_from([(1, 2), (2, 3), (3, 4), (4, 5), (1, 5), (1, 4), (2, 5)])
+    graphlets.append(G)
+
+    # 8 edges
+    G = nx.Graph()
+    G.add_nodes_from([1, 2, 3, 4, 5])
+    G.add_edges_from([(1, 2), (2, 3), (3, 4), (4, 5), (1, 5), (1, 3), (1, 4), (3, 5)])
+    graphlets.append(G)
+
+    G = nx.Graph()
+    G.add_nodes_from([1, 2, 3, 4, 5])
+    G.add_edges_from([(1, 2), (2, 3), (3, 4), (4, 5), (1, 5), (1, 3), (1, 4), (2, 5)])
+    graphlets.append(G)
+
+    # 9 edges
+    G = nx.Graph()
+    G.add_nodes_from([1, 2, 3, 4, 5])
+    G.add_edges_from([(1, 2), (2, 3), (3, 4), (4, 5), (1, 5), (1, 3), (1, 4), (2, 5), (3, 5)])
+    graphlets.append(G)
+
+    # 10 edges
+    G = nx.Graph()
+    G.add_nodes_from([1, 2, 3, 4, 5])
+    G.add_edges_from([(1, 2), (2, 3), (3, 4), (4, 5), (1, 5), (1, 3), (1, 4), (2, 5), (3, 5), (2, 4)])
+    graphlets.append(G)
+
+    return graphlets
