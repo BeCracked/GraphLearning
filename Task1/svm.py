0d15513f942feea5d0956f995d46446f9a954205
Felix Engel <Felix.Justus.Engel@mail.de>
2022-11-14 17:13:17 +0100

    Parallelize gram matrix computation

--- Task1/svm.py
+++ Task1/svm.py
@@ -8,12 +8,14 @@ from scipy.sparse import csr_matrix
 from sklearn import svm
 from sklearn.model_selection import cross_val_score
 
+from multiprocessing import Pool
+
 from Kernels.wl_kernel import wl_kernel
 from Kernels.graphlet_kernel import run_graphlet_kernel
 
 from Kernels.closed_walk_kernel import run_cl_kernel
 
-from typing import Literal
+from typing import Tuple, Literal
 
 warnings.filterwarnings("ignore", category=DeprecationWarning)
 
@@ -39,14 +41,67 @@ def compute_gram_matrix(kern: callable, graph_dataset):
     graph_dataset: passes the graph dataset as a list of graphs
     return: the gram matrix as a numpy object
     """
-    gram_m = np.zeros((len(graph_dataset), len(graph_dataset)))
     print(f"Calculating {len(graph_dataset)} feature vectors...")
     kern_start = time.perf_counter()
     feature_vectors = kern(*graph_dataset)
     kern_end = time.perf_counter()
-    print(f"Calculated feature vectors in {kern_end-kern_start:.4f}s")
+    print(f"Calculated feature vectors in {kern_end - kern_start:.4f}s")
 
     print(f"Constructing gram matrix...")
+    pool = Pool()
+    # Split work along rows
+    graph_start = time.perf_counter()
+    gram_m_par = np.zeros((len(graph_dataset), len(graph_dataset)))
+    f = partial(compute_gram_row_chunk, feature_vectors)
+    row_chunks = pool.map(f, range(len(feature_vectors)))
+    graph_end = time.perf_counter()
+
+    for i, row_chunk in row_chunks:
+        gram_m_par[i] = row_chunk
+
+    print(f"Took {graph_end-graph_start:.2f}s")
+
+    for m in range(1, len(graph_dataset)):
+        for n in range(0, m):
+            gram_m[m][n] = gram_m[n][m]
+
+    save_file(gram_m, "gram_matrix")
+    return gram_m
+
+
+def compute_gram_row_chunk(feature_vectors: list, i: int,
+                           col_min: int = None, col_max: int = None) -> Tuple[int, np.array]:
+    """
+    Only calculates the upper right half for any given row.
+    Parameters
+    ----------
+    i : The index of the row to process.
+    feature_vectors :
+    col_min : Start point in the column.
+    col_max : End point in the column.
+
+    Returns Tuple of row index and the complete row with lower left values as 0.
+    -------
+
+    """
+    if not col_min:
+        col_min = i
+    if not col_max:
+        col_max = len(feature_vectors)
+    j_min, j_max = col_min, col_max
+    fv_1: csr_matrix = feature_vectors[i]
+    gram_row = np.zeros(len(feature_vectors))
+    for j in range(j_min, j_max):
+        fv_2: csr_matrix = feature_vectors[j]
+        d = fv_1 - fv_2
+        dist = d.transpose().dot(d).toarray()[0][0]
+        gram_row[j] = dist
+
+    return i, gram_row
+
+
+def compute_gram_matrix_sync(feature_vectors):
+    gram_m = np.zeros((len(feature_vectors), len(feature_vectors)))
     outer_start = time.perf_counter()
     for i in range(0, len(feature_vectors)):
         fv_1: csr_matrix = feature_vectors[i]
@@ -59,17 +114,10 @@ def compute_gram_matrix(kern: callable, graph_dataset):
 
         if i % 100 == 0:
             outer_end = time.perf_counter()
-            print(f"Outer: {i}/{len(graph_dataset)} ({i/len(graph_dataset)*100:.2f}%) "
+            print(f"Outer: {i}/{len(feature_vectors)} ({i/len(feature_vectors)*100:.2f}%) "
                   f"(Total time: {outer_end-outer_start:.2f}s)")
 
     print(f"Gram matrix construction took {time.perf_counter() - outer_start:.2f}s")
-    for m in range(1, len(graph_dataset)):
-        for n in range(0, m):
-            gram_m[m][n] = gram_m[n][m]
-
-    save_file(gram_m, "gram_matrix")
-    return gram_m
-
 
 def fit(kern: Literal["closed_walk", "graphlet", "WL"], dataset: Literal["DD", "Enzymes", "NCI"]):
     """
@@ -89,7 +137,7 @@ def fit(kern: Literal["closed_walk", "graphlet", "WL"], dataset: Literal["DD", "
         case _:
             print(f"Error: {dataset} is not a valid dataset")
 
-    data = data[:int(len(data)/1)]  # TEMP: Reduce dataset size
+    data = data[:int(len(data) / 1)]  # TEMP: Reduce dataset size
 
     clf = svm.SVC(kernel='precomputed', random_state=42)
     kern_func = None
@@ -109,7 +157,7 @@ def fit(kern: Literal["closed_walk", "graphlet", "WL"], dataset: Literal["DD", "
     scores = cross_val_score(clf, gram_matrix, labels, cv=10)
     print("%0.2f accuracy with a standard deviation of %0.2f" % (scores.mean(), scores.std()))
     fit_end = time.perf_counter()
-    print(f"Fitting took {fit_end-fit_start:.2f}s")
+    print(f"Fitting took {fit_end - fit_start:.2f}s")
 
 
 if __name__ == '__main__':
