7d04ea8c713483b21d94f895919c1f247537189a
Jessica Yu <jessica.yu@rwth-aachen.de>
2022-11-24 23:32:46 +0100

    Add GNetwork to setup Combine GCN layers

---
+++ Task2/GNetwork.py
@@ -0,0 +1,55 @@
+import torch
+
+from GCNLayer import GCNLayer
+
+
+class GNetwork(torch.nn.Module):
+    def __init__(self, input_dim: int, output_dim: int, hidden_dim: int, num_layers: int):
+        """
+        A multilayer perceptron for classification
+
+        Parameters
+        ----------
+        input_dim Dimension of input layer
+        output_dim Dimension of output layer
+        hidden_dim Dimension of hidden layers
+        num_layers Total number of layers
+
+        Returns
+        -------
+
+        """
+        super(GNetwork, self).__init__()
+        self.num_layers = num_layers
+
+        # Setup input layer
+        self.input_layer = GCNLayer(input_dim, hidden_dim)
+
+        # Setup hidden layers in ModuleList
+        self.hidden_layers = torch.nn.ModuleList(
+            [GCNLayer(hidden_dim, hidden_dim) for _ in range(num_layers - 2)]
+        )
+
+        # Setup output layer
+        self.output_layer = GCNLayer(hidden_dim, output_dim)
+
+
+    def forward(self, x):
+        """
+        Forward computation of the GCN Network.
+
+        Parameters
+        ----------
+        x Input vector of GCN Network
+
+        Returns
+        -------
+        Output vector of GCN Network
+        """
+
+        # apply layers
+        y = self.input_layer(x)
+        for i in range(self.num_layers - 2):
+            y = self.hidden_layers[i](y)
+        y = self.output_layer(y)
+        return y
