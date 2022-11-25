ddcddc0a5404a0385316d28c9c42a37d3d9bc201
Jessica Yu <jessica.yu@rwth-aachen.de>
2022-11-25 17:59:41 +0100

    changed dimension parameters in GraphLevelGCN

--- Task2/GraphLevelGCN.py
+++ Task2/GraphLevelGCN.py
@@ -6,15 +6,15 @@ from GMLP import GMLP
 
 
 class GraphLevelGCN(torch.nn.Module):
-    def __init__(self, input_dim: int, hidden_dim: int, num_classes: int):
+    def __init__(self, input_dim: int, output_dim: int, hidden_dim: int):
         """
         Construct the graph-level GCN for according to Exercise 3.
 
         Parameters
         ----------
         input_dim Dimension of input layer for GCN Network
-        hidden_dim Dimension of hidden layer for GCN Network
-        num_classes Number of classes for MLP classification
+        hidden_dim Dimension of hidden layers for GCN Network and MLP Classification
+        output_dim Number of classes for MLP classification
 
         Returns
         -------
@@ -23,10 +23,10 @@ class GraphLevelGCN(torch.nn.Module):
         super(GraphLevelGCN, self).__init__()
 
         # Setup Network of 5 GCN layers and hidden dimension of 64
-        self.GCNNetwork = GNetwork(input_dim, hidden_dim, 64, 5)
+        self.GCNNetwork = GNetwork(input_dim, hidden_dim, hidden_dim, 5)
 
         # Setup MLP classification (one hidden layer of dimension 64, three layers in total)
-        self.MLPClassification = GMLP(hidden_dim, num_classes, 64, 3)
+        self.MLPClassification = GMLP(hidden_dim, output_dim, hidden_dim, 3)
 
     def forward(self, x: torch.Tensor, adj_matrices: torch.Tensor):
         # Apply GCN network
