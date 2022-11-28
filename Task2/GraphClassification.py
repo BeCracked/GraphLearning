539cff68924b3a104585a4b5ffe14cfab7fb3bf2
Felix Engel <Felix.Justus.Engel@mail.de>
2022-11-28 15:57:43 +0100

    Rework GraphClassification

--- Task2/GraphClassification.py
+++ Task2/GraphClassification.py
@@ -1,7 +1,12 @@
+import numpy as np
 import torch
 import pickle
+
+from torch import Tensor
 from torch.utils.data import DataLoader, TensorDataset, SubsetRandomSampler
+from torch.utils.data.dataloader import default_collate
 from sklearn.model_selection import KFold
+from tqdm import tqdm
 
 import preprocessing
 from GraphLevelGCN import GraphLevelGCN
@@ -43,72 +48,129 @@ def load_data(path: str, dataset: str):
     return node_features, norm_matrices, labels, num_labels
 
 
-def run_graph_classification(path: str, dataset_name: str, device: str = "cpu"):
+def run_graph_classification(path: str, dataset_name: str, *,
+                             device: str = "cpu",
+                             epochs=10, batch_size=32, learning_rate=1e-3, fold_count=10) \
+        -> tuple[list, list, list, list]:
+    """
+    Performs k-fold cross validation with the graph classification net on the given dataset.
+
+    Parameters
+    ----------
+    path The path to the dataset file.
+    dataset_name Name of the dataset to use. Either "NCI" or "ENZYMES". Determines certain import behaviour.
+    device The device name to run on.
+    epochs Number of epochs to train for.
+    batch_size Batch sizes for the DataLoaders to use.
+    learning_rate Learning rate to use by the optimizer.
+    fold_count Fold count for k-fold cross validation.
+
+    Returns
+    -------
+    The accuracy results for each fold.
+    Tuple of Lists of the form (train_accs, train_stds, test_accs, test_stds)
+
+    """
     # Extract node features, adjacency matrices, labels and convert to tensors
     x, a, y, num_labels = load_data(path, dataset_name)
 
     # Create dataset and loader for mini batches
     dataset = TensorDataset(x, a, y)
 
-    # TODO: determine epochs and learning and (batch size) rate empirically
-
     # Setup 10-fold cross validation
-    kf = KFold(n_splits=10, random_state=42, shuffle=True)
-    train_accuracy = []
+    kf = KFold(n_splits=fold_count, random_state=42, shuffle=True)
 
-    for train_indices, test_indices in kf.split(dataset, y):
+    results = train_accs, train_stds, test_accs, test_stds = ([], [], [], [])
+    for fold, (train_indices, test_indices) in enumerate(kf.split(dataset, y)):
+        print(f"Running fold {fold + 1}/{fold_count}")
+
         # Extract training and test data for each fold
         train_data_sub = SubsetRandomSampler(train_indices)
         test_data_sub = SubsetRandomSampler(test_indices)
 
-        train_loader = DataLoader(dataset, batch_size=32, sampler=train_data_sub)
-        test_loader = DataLoader(dataset, batch_size=32, sampler=test_data_sub)
+        train_loader = DataLoader(dataset, batch_size=batch_size, sampler=train_data_sub,
+                                  collate_fn=lambda v: tuple(v_.to(device) for v_ in default_collate(v)))
+        test_loader = DataLoader(dataset, batch_size=batch_size, sampler=test_data_sub,
+                                 collate_fn=lambda v: tuple(v_.to(device) for v_ in default_collate(v)))
 
         # Construct neural network and move it to device
-        # Input dimension: length of node vectors, Output dimension: number of labels
+        # Input dimension: length of node vectors
         model = GraphLevelGCN(input_dim=len(x[0][0]), output_dim=num_labels, hidden_dim=64)
         model.train()
         model.to(device)
 
         # Construct optimizer
-        opt = torch.optim.Adam(model.parameters(), lr=0.001)
+        opt = torch.optim.Adam(model.parameters(), lr=learning_rate)
+        loss_fn = torch.nn.functional.cross_entropy
 
         # Training loop
-        for epoch in range(10):
-            for x_train, a_train, y_train in train_loader:
-                # Set gradients to zero
-                opt.zero_grad()
+        train_acc, train_std = 0, 0
+        for epoch in tqdm(range(epochs)):
+            train_acc, train_std = train_loop(train_loader, model, loss_fn, opt)  # Consider only accuracy of last epoch
 
-                # Move data to device
-                x_train = x_train.to(device)
-                a_train = a_train.to(device)
-                y_train = y_train.to(device)
-                # Forward pass and loss
-                y_pred = model(x_train, a_train)
-                loss = torch.nn.functional.cross_entropy(y_pred, y_train)
+        train_accs.append(train_acc)
+        train_stds.append(train_std)
+        test_acc, test_std = test(test_loader, model, loss_fn)
+        test_accs.append(test_acc)
+        test_stds.append(test_std)
 
-                # Backward pass and sgd step
-                loss.backward()
-                opt.step()
+    return results
 
-        # Evaluate fold on test loader
-        num_pred_correct = 0
-        num_pred_total = 0
-        for x_test, a_test, y_test in test_loader:
+
+def train_loop(dataloader, model, loss_fn, optimizer):
+    """
+    Trains the model for one epoch, i.e. on all batches
+
+    Parameters
+    ----------
+    dataloader The dataloader to use. Needs to provide x: node embeddings, a: normalized adjacency matrices, y: labels
+    model The model to train.
+    loss_fn The loss function to use. E.g., torch.nn.functional.cross_entropy
+    optimizer The optimizer to use. E.g., torch.optim.Adam(model.parameters(), lr=learning_rate)
+
+    Returns
+    -------
+    The list of accuracy values for each batch.
+
+    """
+    accuracies = np.zeros(len(dataloader))
+    for batch, (x_train, a_train, y_train) in enumerate(dataloader):
+        # Set gradients to zero
+        optimizer.zero_grad()
+
+        # Forward pass and loss
+        y_pred = model(x_train, a_train)
+        loss = loss_fn(y_pred, y_train)
+
+        # Backward pass and sgd step
+        loss.backward()
+        optimizer.step()
+
+        # Record accuracy
+        accuracies[batch] = get_accuracy(y_pred, y_train)
+
+    return accuracies.mean(), accuracies.std()  # Means-of-Means can be neglected as data will always be same size
+
+
+def test(dataloader, model, loss_fn):
+    accuracies = np.zeros(len(dataloader))
+    with torch.no_grad():
+        for batch, (x_test, a_test, y_test) in enumerate(dataloader):
             y_pred = model(x_test, a_test)
-            num_pred_total += y_test.size(0)
-            # we need the predicted class which are the indices where we have entry 1
-            _, pred_class = torch.max(y_pred, 1)
-            num_pred_correct += torch.sum(pred_class == y_test).item()
-        train_accuracy.append(num_pred_correct / num_pred_total)
-        print("one fold done")
+            accuracies[batch] = get_accuracy(y_pred, y_test)
 
-    test_accuracy = sum(train_accuracy) / len(train_accuracy)
+    return accuracies.mean(), accuracies.std()  # Means-of-Means can be neglected as data will always be same size
 
-    return train_accuracy, test_accuracy
+
+def get_accuracy(pred: Tensor, truth: Tensor) -> float:
+    correct = (pred.argmax(1) == truth).type(torch.float).sum().item()
+    return correct / len(truth)
 
 
 if __name__ == '__main__':
-    train_ac, test_ac = run_graph_classification("../Task1/datasets/NCI1/data.pkl", "NCI", "cpu")
-    print(train_ac)
-    print(test_ac)
+    params = {
+        "epochs": 10, "batch_size": 32, "learning_rate": 1e-3, "fold_count": 10
+    }
+    res = run_graph_classification("../Task1/datasets/NCI1/data.pkl", "NCI", device="cuda", **params)
+
+    print(res)
