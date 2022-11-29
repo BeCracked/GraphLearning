d52b963abd0853e444c4a1390543545e3ac31013
Felix Engel <Felix.Justus.Engel@mail.de>
2022-11-29 11:42:41 +0100

    Add known best trials to hpo

--- Task2/hpo.py
+++ Task2/hpo.py
@@ -19,18 +19,27 @@ def graph_objective(dataset_path: str, dataset_name: str, trial):
     return np.mean(test_accs)
 
 
-def graph_optimization(dataset_path: str, dataset_name: str):
+def graph_optimization(dataset_path: str, dataset_name: str, enqueue_known_best: bool = False):
     objective = partial(graph_objective, dataset_path, dataset_name)
     study = optuna.create_study(direction="maximize", pruner=optuna.pruners.MedianPruner())
 
-    study.optimize(objective, n_trials=10, timeout=600)
-    best_params = study.best_params
+    if enqueue_known_best:
+        match dataset_name:
+            case "NCI":
+                study.enqueue_trial({"epochs": 60, "learning_rate": 0.00641722976851177})  # ~76%
+            case "ENZYMES":
+                study.enqueue_trial({'epochs': 55, 'learning_rate': 0.0015777811296388503})  # 52.61%
+            case _:
+                pass
 
-    return best_params
+    study.optimize(objective, n_trials=20, timeout=600)
+
+    return study.best_params, study.best_value
 
 
 if __name__ == '__main__':
-    # study.enqueue_trial({'epochs': 55, 'learning_rate': 0.005868098030918391})
+    # NCI: {'epochs': 60, 'learning_rate': 0.00641722976851177}
+    #path, dataset = "../Task1/datasets/ENZYMES/data.pkl", "ENZYMES"
     path, dataset = "../Task1/datasets/NCI1/data.pkl", "NCI"
-    best = graph_optimization(path, dataset)
-    print(best)
+    best_param, best_val = graph_optimization(path, dataset, True)
+    print(f"Best found params:{best_param}, Accuracy:{best_val * 100:.2f}%")
