c94837eda543608d4c0e8cfd044dfbf6f96ff61f
Jessica Yu <jessica.yu@rwth-aachen.de>
2022-12-04 20:03:43 +0100

    Added GNN layer

--- Task3/Modules/sparse_gnn_layer.py
+++ Task3/Modules/sparse_gnn_layer.py
@@ -1,11 +1,45 @@
 from typing import Literal
 
 import torch
+import torch_scatter
 
 
 class SparseGNNLayer(torch.Module):
-    def __init__(self, dim_in: int, dim_out: int, *, aggregation: Literal["SUM", "MEAN", "MAX"] | str):
-        raise NotImplementedError
+    def __init__(self, M_dim_in: int, M_dim_out: int, U_dim_in: int, U_dim_out: int, *, aggregation: Literal["SUM", "MEAN", "MAX"] | str):
+        super().__init__()
 
-    def forward(self, x: torch.Tensor):
-        return x
+        self.aggregation = aggregation
+
+        # Setup functions M and U TODO: Apply MLP? Do we need hidden layers?
+        self.M = torch.nn.Sequential(
+            torch.nn.Linear(M_dim_in, M_dim_out),
+            torch.nn.ReLU()
+        )
+
+        self.U = torch.nn.Sequential(
+            torch.nn.Linear(U_dim_in, U_dim_out),
+            torch.nn.ReLU()
+        )
+
+    def forward(self, H: torch.Tensor, Xe: torch.Tensor, id_Xe: torch.Tensor):
+        # Extract features according to indices
+        features = torch.index_select(H, dim=0, index=id_Xe[0])
+        # Concatenate along feature dimension
+        M_con_cat = torch.cat([features, Xe], dim=1)
+        # Apply MLP
+        Y = self.M(M_con_cat)
+
+        # apply aggregation
+        match self.aggregation:
+            case "SUM":
+                Z = torch_scatter.scatter_add(Y, index=id_Xe[1], dim=0)
+            case "MEAN":
+                Z = torch_scatter.scatter_mean(Y, index=id_Xe[1], dim=0)
+            case "MAX":
+                Z = torch_scatter.scatter_max(Y, index=id_Xe[1], dim=0)[0]
+
+        # Concatenate along feature dimension
+        U_con_cat = torch.cat([H, Z], dim=1)
+        # Apply MLP
+        H_next = self.U(U_con_cat)
+        return H_next
