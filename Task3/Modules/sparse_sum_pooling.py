4e266ea1bbac385b7f72f0f8f13c226d3fa377a0
Jessica Yu <jessica.yu@rwth-aachen.de>
2022-12-04 22:17:41 +0100

    Added Sum Pooling

--- Task3/Modules/sparse_sum_pooling.py
+++ Task3/Modules/sparse_sum_pooling.py
@@ -1,6 +1,11 @@
 import torch
+import torch_scatter
 
 
 class SparseSumPooling(torch.Module):
     def __init__(self):
-        raise NotImplementedError
+        super().__init__()
+
+    def forward(self, H: torch.Tensor, batch_idx: torch.Tensor):
+        # TODO: is that all?
+        return torch_scatter.scatter(H, index=batch_idx, dim=0, reduce='sum')
