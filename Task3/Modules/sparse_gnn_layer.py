4df62b6e751b31a62e8f2f4da7ed5b7334651a53
Jessica Yu <jessica.yu@rwth-aachen.de>
2022-12-05 19:01:26 +0100

    Added dropout to GNN layer

--- Task3/Modules/sparse_gnn_layer.py
+++ Task3/Modules/sparse_gnn_layer.py
@@ -5,7 +5,7 @@ import torch_scatter
 
 
 class SparseGNNLayer(torch.nn.Module):
-    def __init__(self, M_dim_in: int, M_dim_out: int, U_dim_in: int, U_dim_out: int, *, aggregation: Literal["SUM", "MEAN", "MAX"] | str):
+    def __init__(self, M_dim_in: int, M_dim_out: int, U_dim_in: int, U_dim_out: int, *, aggregation: Literal["SUM", "MEAN", "MAX"], drop_prob: float):
         super().__init__()
 
         self.aggregation = aggregation
@@ -21,6 +21,9 @@ class SparseGNNLayer(torch.nn.Module):
             torch.nn.ReLU()
         )
 
+        # Dropout probability
+        self.drop_prob = drop_prob
+
     def forward(self, H: torch.Tensor, Xe: torch.Tensor, id_Xe: torch.Tensor):
         # Extract features according to indices
         features = torch.index_select(H, dim=0, index=id_Xe[0])
@@ -31,16 +34,18 @@ class SparseGNNLayer(torch.nn.Module):
 
         # apply aggregation
         match self.aggregation:
-            case "SUM":
-                Z = torch_scatter.scatter_add(Y, index=id_Xe[1], dim=0)
             case "MEAN":
                 Z = torch_scatter.scatter_mean(Y, index=id_Xe[1], dim=0)
             case "MAX":
                 Z = torch_scatter.scatter_max(Y, index=id_Xe[1], dim=0)[0]
+            case "SUM":
+                Z = torch_scatter.scatter_add(Y, index=id_Xe[1], dim=0)
 
         # Concatenate along feature dimension
         U_con_cat = torch.cat([H, Z], dim=1)
         # Apply MLP
         H_next = self.U(U_con_cat)
+        # Apply dropout
+        H_drop = torch.nn.functional.dropout(H_next, p=self.drop_prob, training=self.training)
 
-        return H_next
+        return H_drop
