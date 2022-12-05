6666108feb191ccf3aa537517623255ee034f3e1
Felix Engel <Felix.Justus.Engel@mail.de>
2022-12-05 19:03:43 +0100

    Update edge label transform

--- Task3/DataHandling/preprocessing.py
+++ Task3/DataHandling/preprocessing.py
@@ -3,21 +3,33 @@ import networkx as nx
 from typing import List
 
 
-def transform_edge_labels(graphs: List[nx.Graph]):
+def edge_labels_to_one_hot(graphs: List[nx.Graph], *, edge_feature_key="edge_label") -> List[nx.Graph]:
+    """
+
+    Parameters
+    ----------
+    graphs
+    edge_feature_key
+
+    Returns
+    -------
+    A copy of graphs with the edge labels transformed to a one hot encoding representation.
+
+    """
     edge_labels = get_edge_labels(graphs)
     new_graphs = []
     for graph in graphs:
         new_labels = dict()
         for edge in graph.edges(data=True):
-            new_labels[(edge[0], edge[1])] = edge[2]["edge_label"]
+            new_labels[(edge[0], edge[1])] = edge[2][edge_feature_key]
 
         for (v, w), value in new_labels.items():
             one_hot = [0] * len(edge_labels)
             one_hot[edge_labels.index(new_labels[(v, w)])] = 1
             new_labels[(v, w)] = one_hot
 
-        new_graph = graph
-        nx.set_edge_attributes(new_graph, new_labels, name="edge_label")
+        new_graph = graph.copy()
+        nx.set_edge_attributes(new_graph, new_labels, name=edge_feature_key)
         new_graphs.append(new_graph)
 
     return new_graphs
@@ -36,4 +48,4 @@ if __name__ == '__main__':
     with open("../datasets/ZINC_Train/data.pkl", "rb") as f:
         data = pickle.load(f)
 
-    transform_edge_labels(data)
+    edge_labels_to_one_hot(data)
