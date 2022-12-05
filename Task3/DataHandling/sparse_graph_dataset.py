6988ff76ba54fa3d8e73bf090d6788d3b06a7c80
Felix Engel <Felix.Justus.Engel@mail.de>
2022-12-05 19:58:43 +0100

    Implement sparse_graph_collation function

--- Task3/DataHandling/sparse_graph_dataset.py
+++ Task3/DataHandling/sparse_graph_dataset.py
@@ -47,7 +47,7 @@ class SparseGraphDataset(Dataset):
 
             # Graph Labels
             if graph_feature_key:
-                self.graph_labels[i] = g.graph[graph_feature_key]
+                self.graph_labels[i] = torch.tensor(g.graph[graph_feature_key])
 
     def __getitem__(self, item) -> tuple[torch.Tensor, torch.Tensor, torch.Tensor, torch.Tensor]:
         return SparseGraphRep(self.edge_lists[item], self.node_features[item],
@@ -57,14 +57,48 @@ class SparseGraphDataset(Dataset):
         return self.graph_count
 
 
-def sparse_graph_collation(datasets: list[SparseGraphDataset]):
-    raise NotImplementedError
+def sparse_graph_collation(sparse_reps: list[SparseGraphRep]):
+    edge_features_flag: bool = sparse_reps[0].edge_features is not None and sparse_reps[0].edge_features._nnz() > 0
+
+    edge_list = torch.cat([r.edge_list for r in sparse_reps], dim=1)
+    node_features = torch.cat([r.node_features for r in sparse_reps])
+    edge_features = None
+    if edge_features_flag:
+        edge_features = torch.cat([r.edge_features for r in sparse_reps])
+    graph_labels = torch.cat([r.graph_label for r in sparse_reps])
+
+    total_node_count = sum((len(r.node_features) for r in sparse_reps))
+    batch_idx = torch.zeros(total_node_count, dtype=torch.int)
+    b_i = 0  # Last position in the batch_idx vector
+
+    for g_idx, rep in enumerate(sparse_reps):
+        start, end = b_i, b_i + rep.node_features.shape[0]
+        for i in range(start, end):
+            batch_idx[i] = int(g_idx)
+        b_i = end
+
+    collated_rep = SparseGraphRep(edge_list, node_features, edge_features, graph_labels)
+
+    return batch_idx, *collated_rep
 
 
 if __name__ == '__main__':
+    import time
+    from Task3.configurations import zinc_base_params
+    from Task3.DataHandling.preprocessing import edge_labels_to_one_hot
+    s = time.perf_counter()
     path = "../datasets/ZINC_Train/data.pkl"
     with open(path, 'rb') as f:
-        data = pickle.load(f)
+        data = edge_labels_to_one_hot(pickle.load(f))
 
-    dataset = SparseGraphDataset(data, edge_feature_key="edge_label")
+    dataset = SparseGraphDataset(data, **zinc_base_params)
+    reps = []
+    for rp in dataset:
+        reps.append(rp)
+    sparse_graph_collation(reps)
     dl = DataLoader(dataset, collate_fn=sparse_graph_collation)
+
+    e = time.perf_counter()
+
+    print(f"Took {e-s}")
+    print(dl)
