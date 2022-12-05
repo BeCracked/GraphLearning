57974bb26f9fb004610de5cd5bfeff72a57e08df
Felix Engel <Felix.Justus.Engel@mail.de>
2022-12-05 16:58:09 +0100

    Add SparseGraphRep namedtuple

--- Task3/DataHandling/sparse_graph_dataset.py
+++ Task3/DataHandling/sparse_graph_dataset.py
@@ -1,14 +1,22 @@
 import pickle
+from typing import Optional
 
 import torch
 from torch.utils.data import Dataset, DataLoader
 import networkx as nx
 
+from collections import namedtuple
+
+SparseGraphRep = namedtuple(
+    "SparseGraphRep",
+    ["edge_list", "node_features", "edge_features", "graph_label"]
+)
+
 
 class SparseGraphDataset(Dataset):
     def __init__(self, graphs: list[nx.Graph], *,
                  device: str | None = None,
-                 node_feature_key: str | None = "node_label", edge_feature_key: str | None = None,
+                 node_feature_key: str = "node_label", edge_feature_key: Optional[str] = None,
                  graph_feature_key: str | None = "label",
                  **kwargs):
         super(SparseGraphDataset, self).__init__()
@@ -26,25 +34,26 @@ class SparseGraphDataset(Dataset):
             i: int
             # Node features
             if node_feature_key:
-                self.node_features[i] = torch.tensor([val for node, val in g.nodes(data=node_feature_key)])
+                self.node_features[i] = torch.tensor(
+                    [val for node, val in g.nodes(data=node_feature_key)], device=device)
 
             # Directed Edge List
             s, t, v = map(list, zip(*g.to_directed().edges(data=edge_feature_key)))  # source, target, value
-            idx = torch.tensor([s, t])
-            shape = (2, len(v))
+            idx = torch.tensor([s, t], device=device)
             self.edge_lists[i] = idx
 
             if edge_feature_key:
-                self.edge_features[i] = torch.tensor(v).to_sparse_coo()
+                self.edge_features[i] = torch.tensor(v, device=device).to_sparse_coo()
 
             # Graph Labels
             if graph_feature_key:
                 self.graph_labels[i] = g.graph[graph_feature_key]
 
-    def __getitem__(self, item):
-        return self.edge_lists[item], self.node_features[item], self.edge_features[item], self.graph_labels[item]
+    def __getitem__(self, item) -> tuple[torch.Tensor, torch.Tensor, torch.Tensor, torch.Tensor]:
+        return SparseGraphRep(self.edge_lists[item], self.node_features[item],
+                              self.edge_features[item], self.graph_labels[item])
 
-    def __len__(self):
+    def __len__(self) -> int:
         return self.graph_count
 
 
